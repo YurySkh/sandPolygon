4bb411b44fd4f1dc32c509917c13aedc6519dc2c
yurysukhan <yurysukhan@gmail.com>
2024-03-21 01:07:26 +0300

    Get number from string

---
+++ codewars/8kuy/get-number-from-string.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+//SOLUTION:
+function getNumberFromString(s) {
+	return +s.replace(/\D/g, "");
+ }
